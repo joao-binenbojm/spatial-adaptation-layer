e7497d2adf21a0ce6dd3b79f19b1e828998b3737
joao-binenbojm <jbdsp.2@gmail.com>
2025-05-13 16:54:37 +0100

    Made another update on create conditions to account for HPC limit of 50 jobs

--- sal_classification/create_conditions.py
+++ sal_classification/create_conditions.py
@@ -1,10 +1,11 @@
 import json
-
+import os
 
 with open('sal_classification/exp-intra.json', 'rb') as f:
     exp = json.load(f)
 
 conditions_dir = 'conditions1'
+os.makedirs(f"sal_classification/{conditions_dir}", exist_ok=True)
 
 nconditions = 1
 exp['network'] = 'CapgMyoNet'
@@ -22,6 +23,8 @@ for num_epochs in [1, 5, 10]:
                 nconditions += 1
                 if nconditions % 49 == 0:
                     conditions_dir = conditions_dir[:-1] + str(int(conditions_dir[-1]) + 1) # increases condition dir
+                    os.makedirs(f"sal_classification/{conditions_dir}", exist_ok=True)
+
 
 
 
@@ -35,9 +38,11 @@ for num_epochs in [15, 30, 50]:
             for p_input in [0.0, 0.25, 0.5, 0.75]:
                 exp['p_input'] = p_input
                 exp['name'] = f"logreg_{num_epochs}_{int(rbase)}_{int(median_filter)}_{p_input}"
-                with open(f"sal_classification/conditions/{nconditions}.json", 'w') as f:
+                with open(f"sal_classification/{conditions_dir}/{nconditions}.json", 'w') as f:
                     json.dump(exp, f)
                 nconditions += 1
                 if nconditions % 49 == 0:
                     conditions_dir = conditions_dir[:-1] + str(int(conditions_dir[-1]) + 1) # increases condition dir
+                    os.makedirs(f"sal_classification/{conditions_dir}", exist_ok=True)
+
 
